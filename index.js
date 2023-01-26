a1ed0dd0a447368245260e3245baae5fb2fdd355
Martin Ferreira <martinferreiraolate@gmail.com>
2023-01-26 20:22:55 -0300

    First commit :D

---
+++ index.js
@@ -0,0 +1,6 @@
+const promedy = require("./promedio.js");
+
+const weight = 100; // En KG
+const reps = 10;
+
+console.log(promedy(weight, reps));
---
+++ promedio.js
@@ -0,0 +1,28 @@
+const epley = (weight, reps) => {
+    return weight * (1 + reps / 30.0);
+}
+
+const brzycki = (weight, reps) => {
+    return weight * (36.0 / (37.0 - reps));
+}
+
+const lombardi = (weight, reps) => {
+    return weight * reps ** 0.10;
+}
+
+const mayhew = (weight, reps) => {
+    return 100 * weight / (52.2 + 41.9 * Math.E ** (-0.055 * reps));
+}
+
+const oconner = (weight, reps) => {
+    return weight * (1 + reps / 40.0);
+}
+
+module.exports = (weight, reps) => {
+    if (isNaN(weight) || isNaN(reps)) {
+        throw new TypeError('weight and reps must be numbers')
+    } else {
+        const promedy = (epley(weight, reps) + brzycki(weight, reps) + lombardi(weight, reps) + mayhew(weight, reps) + oconner(weight, reps)) / 5.0;
+        return promedy;
+    }
+}
